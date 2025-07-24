2d0f936e0955cb48ea6527279b3d641e7671a9d1
danninx <public.danninx@gmail.com>
2025-07-24 18:47:57 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -7,19 +7,39 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.development = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go 
-        ];
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.development = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        go 
+      ];
 
-        env = {
-          STARSHIP_NIX_SHELL_NAME = "tim";
-        };
+      env = {
+        STARSHIP_NIX_SHELL_NAME = "tim";
       };
-    });
+    };
+
+    packages.default = pkgs.buildGo124Module {
+      pname = "tim";
+      version = "0.3.0";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "danninx";
+        repo = "tim";
+        rev = "fd183ede4038b82822c611e9d8687124d95424ed";
+        hash = "sha256-IjgDz7wBzDLK7ae0HNfrOOUuup3xTA4LA1GAFx4eGOs=";
+      };
+
+      vendorHash = "sha256-GkwY1Y8n7vOJ2VFMjZP3Aew65HIPxQ/hb2eY2wq7rmE=";
+
+      meta = {
+        description = "templating script for common sources";
+        license = pkgs.lib.licenses.mit;
+        homepage = "https://github.com/danninx/tim";
+      };
+    };
+  });
 }
